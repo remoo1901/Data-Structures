3e3fe4509d106acc3743af8d8b7fe1568f412f2a
Remon Abdou <remoo1901@hotmail.com>
2020-08-21 22:12:42 -0400

    doublyLinkedList completed

--- queue/queue.py
+++ queue/queue.py
@@ -15,7 +15,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
 from singly_linked_list import Node
 from singly_linked_list import LinkedList
 
-""" class Queue:
+class Queue:
     def __init__(self):
         self.size = 0
         self.storage = []
@@ -30,7 +30,7 @@ from singly_linked_list import LinkedList
         else:
             self.size -= 1
             return self.storage.pop(0)
- """
+ 
 
 
 class Queue:
